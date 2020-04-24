69a8b650e7229de815b905a0a87f939daabcef22
qiyizhong <qiyizhong@bytedance.com>
2020-04-24 14:19:59 +0800

    Initial Commit

---
+++ scroll_animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scroll_animation
+//
+//  Created by qiyizhong on 2020/4/24.
+//  Copyright © 2020 qiyizhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
